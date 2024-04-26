ca2eb78f186b7e85f5a3df78d1083bb73b2fddb1
w3schools-test <teresiahwambuikaberia@gmail.com>
2024-04-27 00:55:05 +0300

    add new changes for review

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import fact_page, student_page, image_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-  path('', views.index)
+    path('fact/', fact_page, name='fact'),
+    path('student/', student_page, name='student'),
+    path('image/', image_page, name='image'),
 ]
+
--- django_project/views.py
+++ django_project/views.py
@@ -1,20 +1,23 @@
 import requests
 from django.shortcuts import render 
+import random
 
-def index(request):
-  response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
-  data = response.json()
-  fact = data['text']
+def fact_page(request):
+    response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+    data = response.json()
+    fact = data['text']
+    return render(request, 'fact_page.html', {'fact': fact})
 
-  r3 = requests.get('https://dog.ceo/api/breeds/image/random')
-  res3 = r3.json()
-  dog = res3['message']
+def student_page(request):
+    response2 = requests.get('https://freetestapi.com/api/v1/students')
+    data2 = response2.json()
+    random_student = random.choice(data2)['name']
+    return render(request, 'student_page.html', {'name': random_student})
 
-  # This is the assignment for the Hackathon, 
-  # Instructions: 
-  # Use this API and randomize the students
-  response2 = requests.get('https://freetestapi.com/api/v1/students') # Use this API
-  data2 = response2.json()
-  name = data2[0]['name']
+def image_page(request):
+    r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+    res3 = r3.json()
+    dog = res3['message']
+    return render(request, 'image_page.html', {'dog': dog})
+
   
-  return render(request, 'templates/index.html', {'fact': fact, 'dog': dog,  'name': name})
